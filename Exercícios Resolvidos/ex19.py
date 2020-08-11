eb1df15b424e2fbe113b74938c84e75ac54c0d36
Rarantes <51489859+rarants@users.noreply.github.com>
2020-08-11 19:58:10 -0300

    Update ex19.py

--- Exercícios Resolvidos/ex19.py
+++ Exercícios Resolvidos/ex19.py
@@ -1,4 +1,4 @@
-# 19. Faça um programa em que o usuário deve inserir nomes em uma lista até que o mesmo digite "0".  A seguit, imprima a mesma exibindo seus 
+# 19. Faça um programa em que o usuário deve inserir nomes em uma lista até que o mesmo digite "0".  A seguir, imprima a mesma exibindo seus 
 # valores e respectivos índices.
 nome = input("Digite um nome: ")
 lista=[]
